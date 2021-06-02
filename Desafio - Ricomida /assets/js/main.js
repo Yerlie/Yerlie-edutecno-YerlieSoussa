b5c26a6376b8d4c05f65d1f796c977129d4658f6
Yerlie <yerliesoussa@gmail.com>
2021-06-01 21:49:30 -0400

    Desafío - GIGEO App, Desafío - Revista online

--- Desafio - Ricomida /assets/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-  $("#carouselExampleSlidesOnly").carousel();
-  $("#boton1").click(function () {
-      alert("El correo fue enviado correctamente...");
-  })
-});
